be6037c93060158ab2c5d087f04e54509b56f5b3
Sune1998 <53908735+Sune1998@users.noreply.github.com>
2019-09-06 13:34:49 +0200

    Add files via upload

--- opgave 4/src/com/company/Main.java
+++ opgave 4/src/com/company/Main.java
@@ -4,15 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// løsning til opgave 4
-String[] columNames = {" a",
-        "a^2",
-        "a^3",
-        "a^4" };
-int[] data = { 1,
-        1,
-        1,
-        1,
+        System.out.println("a   a^2   a^3  a^4");
+        System.out.println("1    1     1    1");
+        System.out.println("2    4     8    16");
+        System.out.println("3    9     27    61");
+        System.out.println("4    16    64   256");
 
-};
     }
 }
