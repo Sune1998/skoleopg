dea889be95957e17e788ef4c52f44ef0bc8ed39d
Sune1998 <53908735+Sune1998@users.noreply.github.com>
2019-09-06 13:47:12 +0200

    løsning til opg 5

---
+++ opgave5/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// løsning til opgave 5
+        System.out.print("(7.5 * 6.5 - 4.5 *3) / (47.5 - 5.5) = ");
+        System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5));
+    }
+}
